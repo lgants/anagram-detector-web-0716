19f496ca2543e62f84fded6750cb8986ab00c5e4
lgants <lgants@gmail.com>
2016-07-27 23:50:35 -0400

    anagram detector lab complete -ltg

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require 'pry'
+
+
+class Anagram
+  attr_accessor :word, :possible_anagrams
+
+  def initialize(input_word)
+    @word = input_word
+  end
+
+  def match(possible_anagrams)
+    results_array = Array.new
+    possible_anagrams.each do |element|
+      if element.split("").sort == word.split("").sort
+        results_array << element
+      end
+    end
+    results_array
+  end
+end
